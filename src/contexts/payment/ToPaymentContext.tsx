8ad3f5c940f136c9172f2ac26d1bb3035e6d7c0c
Matheus Augusto Fraga Correa <matheusguto1@hotmail.com>
2025-10-13 17:15:20 -0300

    criando contexto de link para pagamento do curso

---
+++ src/contexts/payment/ToPaymentContext.tsx
@@ -0,0 +1,27 @@
+import { createContext } from "react";
+
+    interface IToPaymentProp{
+        toLink: ()=>void;
+    }
+
+    const PayContext = createContext({} as IToPaymentProp);
+
+    interface IPaymentProvider{
+        children: React.ReactNode;
+    }
+
+    export const PayContextProvider: React.FC<IPaymentProvider>=({children})=>{
+        const paymentUrl = "https://pay.kirvano.com/2d8a4f30-1f2b-4bc5-acfc-a57f04ee5913"
+
+        const toLink = ()=>{
+            window.open(paymentUrl, "_blank")
+        }
+        return(
+            <PayContext.Provider value={{toLink}}>
+                {children}
+            </PayContext.Provider>
+        );
+    }
+
+     
+
