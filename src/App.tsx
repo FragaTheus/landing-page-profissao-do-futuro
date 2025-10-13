4592095c0e76645f51481a4059918d7b040c6f7a
Matheus Augusto Fraga Correa <matheusguto1@hotmail.com>
2025-10-13 17:19:11 -0300

    fornecendo context para aplicacao

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
+import { PayContextProvider } from "./contexts/payment/ToPaymentContext";
 import { ResContextProvider } from "./contexts/responsive-context/ResponsiveContext";
 import { PageLayout } from "./layouts/page-layout/PageLayout";
 
 function App() {
   return (
+    <PayContextProvider>
     <ResContextProvider>
       <PageLayout />
     </ResContextProvider>
+    </PayContextProvider>
   );
 }
 
